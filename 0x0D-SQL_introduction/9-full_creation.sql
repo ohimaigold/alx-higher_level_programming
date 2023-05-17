a22c29d5e1b59d1f39fc27d709b442f4824af9f0
Ogedegbe O Eric <ohimaigold@gmail.com>
2023-05-16 20:03:50 -0700

    0x0D. SQL - Introduction

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,6 @@
--- A script that creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows.
+-- A script that creates a table second_table in the database hbtn_0c_0.
 
-EATE TABLE IF NOT EXISTS second_table (id INT,
+CREATE TABLE IF NOT EXISTS second_table (id INT, 
 name VARCHAR(256),
 score INT);
 
