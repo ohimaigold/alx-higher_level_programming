3dbbd25df4b5c9dabdb8fad91f530bab6f0a0355
Ogedegbe O Eric <ohimaigold@gmail.com>
2023-05-17 09:59:15 -0700

    0x0E. SQL - More queries

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,2 +1,4 @@
---A script that creates the table id_not_null on your MySQL server.
+-- script that creates the table id_not_null
+-- id_not_null description:  ID Name
+
 CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1 NOT NULL , name VARCHAR(256));
