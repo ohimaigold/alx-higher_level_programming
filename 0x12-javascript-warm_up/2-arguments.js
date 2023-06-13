58dd8285cff2b77d64e2f1b4adc1e2851a4c47de
Ogedegbe O Eric <ohimaigold@gmail.com>
2023-06-13 09:33:30 -0700

    0x12. JavaScript - Warm up

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
-const value = process.argv[2];
-if (value === undefined) {
+
+if (process.argv.length <= 2) {
   console.log('No argument');
+} else if (process.argv.length <= 3) {
+  console.log('Argument found');
 } else {
   console.log('Arguments found');
 }
