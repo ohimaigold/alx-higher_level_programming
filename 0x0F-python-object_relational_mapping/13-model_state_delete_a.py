08bd2f0a923290f191d99d0cc05d35f70b0fea29
Ogedegbe O Eric <ohimaigold@gmail.com>
2023-06-20 12:38:58 -0700

    0x0F. Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 """
 Deletes all State objects with a name containing
-the letter a from the database hbtn_0e_6_usa.
-Usage: ./13-model_state_delete_a.py <mysql username> /
-                                    <mysql password> /
-                                    <database name>
+the letter a from the database hbtn_0e_6_usa
+
 """
 
 from sys import argv
 from model_state import Base, State
 from sqlalchemy import create_engine
+from model_state import State
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
